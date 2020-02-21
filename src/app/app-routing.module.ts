90a3c1f2fdec7e1001b499b5d8ec0f24902b6f03
A121 <atulpisal.ap@gmail.com>
2020-02-21 19:51:29 +0530

    have some changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { AuthGuard } from './auth.guard';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { EventsComponent } from './events/events.component';
 import { RegisterComponent } from './register/register.component';
+import { EventsComponent } from './events/events.component';
 import { SpecialEventsComponent } from './special-events/special-events.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   {
