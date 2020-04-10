92aa89447fce0b9f27ec4507431cd490cd6c85f4
a121 <shankar.pisal@codevian.com>
2020-04-10 10:02:44 +0530

    debouncing search inangular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { AuthService } from './auth.service';
+import { fromEvent } from 'rxjs';
+import { filter, debounceTime, distinctUntilChanged, tap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,8 @@ import { AuthService } from './auth.service';
 })
 export class AppComponent {
   title = 'Shopping';
+  @ViewChild('input',{static: false}) input: ElementRef;
+
   constructor(public _authService : AuthService){}
 
   onActivate(event){
@@ -15,4 +19,18 @@ export class AppComponent {
     // alert(event);
   }
 
+  ngAfterViewInit() {
+    // server-side search
+    fromEvent(this.input.nativeElement,'keyup')
+        .pipe(
+            map(event=>event),
+            filter(Boolean),
+            debounceTime(1000),
+            distinctUntilChanged(),
+            tap((text) => {
+              console.log("------ value : ",this.input.nativeElement.value);
+            })
+        )
+        .subscribe();
+    }
 }
